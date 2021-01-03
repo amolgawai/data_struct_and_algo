15b2239f47f5246c6a8dcf091bb1c619185e9105
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2021-01-03 20:07:07 +0100

    added quick union and respective test

--- coursera_algo1/test_union_find.py
+++ coursera_algo1/test_union_find.py
@@ -1,6 +1,6 @@
 """Tests for Union Find"""
 import os
-from coursera_algo1.union_find import QuickFindUF
+from coursera_algo1.union_find import QuickFindUF, QuickUnionUF
 
 
 FIXTURE_DIR = os.path.join(
@@ -28,8 +28,6 @@ def get_test_input(input_f_name):
 
 
 def test_quickfind_qf():
-    """Main function for testing"""
-
     input_fl_name = os.path.join(FIXTURE_DIR, "tinyUF.txt")
     test_sz, test_unions = get_test_input(input_fl_name)
     qf_uf = QuickFindUF(test_sz)
@@ -37,6 +35,17 @@ def test_quickfind_qf():
     for xy_tpl in test_unions:
         qf_uf.union(xy_tpl[0], xy_tpl[1])
 
-    print(qf_uf)
     for test_tpl in test_unions:
         assert qf_uf.are_connected(test_tpl[0], test_tpl[1])
+
+
+def test_quickunion_qf():
+    input_f_name = os.path.join(FIXTURE_DIR, "mediumUF.txt")
+    test_sz, test_unions = get_test_input(input_f_name)
+    test_uf = QuickUnionUF(test_sz)
+
+    for xy_tpl in test_unions:
+        test_uf.union(xy_tpl[0], xy_tpl[1])
+
+    for test_tpl in test_unions:
+        assert test_uf.are_connected(test_tpl[0], test_tpl[1])
--- coursera_algo1/union_find.py
+++ coursera_algo1/union_find.py
@@ -2,7 +2,15 @@
 
 
 class QuickFindUF:
-    """Quick Find Union Find implementation"""
+    """Quick Find Union Find implementation
+
+    Simple implementation suitable for small data set.
+
+    The data is represented as an array of ids, index of array is a node
+    The nodes with same ids are connected.
+    Union can take a lot of time as it needs to find all the items with same
+    ids by iterating through the entire array
+    """
 
     def __init__(self, size):
         self.ids = list(i for i in range(size))
@@ -25,8 +33,57 @@ class QuickFindUF:
         id_1 -- first id
         id_2 -- second id
         """
+
+        if self.are_connected(id_1, id_2):
+            return
         first = self.ids[id_1]
         second = self.ids[id_2]
         for indx, id_num in enumerate(self.ids):
             if id_num == first:
                 self.ids[indx] = second
+
+
+class QuickUnionUF:
+    """Quick Union Union Find implementation
+
+    The data is represented as an array as indexes as nodes.
+    Each node is also a leaf of a connection tree. Some nodes are root.
+    The id of each node represents the root node.
+    """
+
+    def __init__(self, size):
+        self.ids = list(i for i in range(size))
+
+    def __str__(self):
+        """Pretty print class"""
+        return f"{self.ids}"
+
+    def _root(self, i_d):
+        """Find the root of id
+        Keyword Arguments:
+        id -- for which root is needed
+        """
+
+        while i_d != self.ids[i_d]:
+            i_d = self.ids[i_d]
+
+        return i_d
+
+    def are_connected(self, id_1, id_2):
+        """Retrurns Ttue if p and q are connected
+        Keyword Arguments:
+        id_1 -- first id
+        id_2 -- second id
+        """
+        return self._root(id_1) == self._root(id_2)
+
+    def union(self, id_1, id_2):
+        """Connects (unioins) two ids
+        Keyword Arguments:
+        id_1 -- first id
+        id_2 -- second id
+        """
+
+        first = self._root(id_1)
+        second = self._root(id_2)
+        self.ids[first] = second
