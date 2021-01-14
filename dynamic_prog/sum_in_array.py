a86c0e6a7859ec6c33663bec0020c04e8201b484
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2021-01-14 22:26:22 +0100

    started with sum in array dynamic programming problems

---
+++ dynamic_prog/sum_in_array.py
@@ -0,0 +1,122 @@
+""" Vaiety of functions that are related to finding sum of a target
+number from a given array
+"""
+from memoization import cached
+
+
+@cached
+def can_sum_mem(target_sum, num_tpl):
+    """Finds if the sum can be constructed from numbers in the array
+    Keyword Arguments:
+    target_sum -- the taget sum that needs to be constructed
+    num_lst    -- the tuples describing array of numbers
+    """
+
+    if target_sum == 0:
+        return True
+    if target_sum < 0:
+        return False
+
+    for a_num in num_tpl:
+        if can_sum_mem(target_sum - a_num, num_tpl):
+            return True
+
+    return False
+
+
+def can_sum_tabl(target_sum, num_tpl):
+    """ Tabulation implementation of can_sum"""
+
+    tbl = [False] * (target_sum + 1)
+    tbl[0] = True
+
+    for indx, a_tbl in enumerate(tbl):
+        if a_tbl:
+            for a_num in num_tpl:
+                if indx + a_num <= target_sum:
+                    tbl[indx + a_num] = True
+
+    return tbl[-1]
+
+
+@cached
+def how_sum_mem(target_sum, num_tpl):
+    """returns a sub array of num_tpl sum of which equals target_sum
+    returns a None in case no sum can be found
+    """
+
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+
+    for a_num in num_tpl:
+        res = how_sum_mem(target_sum - a_num, num_tpl)
+        if res is not None:
+            res.append(a_num)
+            return res
+
+    return None
+
+
+def how_sum_tabl(target_sum, num_tpl):
+    """Tabulation implementation of how sum"""
+
+    tbl = [None] * (target_sum + 1)
+    tbl[0] = []
+
+    for indx in range(target_sum + 1):
+        a_tbl = tbl[indx]
+        if a_tbl is not None:
+            for a_num in num_tpl:
+                if indx + a_num <= target_sum:
+                    tbl[indx + a_num] = a_tbl + [a_num]
+
+    return tbl[-1]
+
+
+@cached
+def best_sum_mem(target_sum, num_tpl):
+    """returns a smallest sub array of num_tpl sum of which equals target_sum
+    returns a None array in case no sum can be found
+    """
+
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+
+    shortest_combo = None
+    for a_num in num_tpl:
+        res = how_sum_mem(target_sum - a_num, num_tpl)
+        if res is not None:
+            sum_combo = res + [a_num]
+            if not shortest_combo or len(sum_combo) < len(shortest_combo):
+                shortest_combo = list(sum_combo)
+
+    return shortest_combo
+
+
+def run_algo(input_tpl_lst, algo_func):  # pragma: no cover
+    """Runs an algorithm and prints output"""
+
+    for input_tpl in input_tpl_lst:
+        print(algo_func(input_tpl[0], input_tpl[1]))
+
+    print("=================")
+
+
+def main():  # pragma: no cover
+    """Manual testing"""
+
+    inputs = ((7, (3, 4, 5, 7)), (7, (7, 3, 4, 5)), (300, (7, 14)))
+
+    run_algo(inputs, can_sum_mem)
+    run_algo(inputs, can_sum_tabl)
+    run_algo(inputs, how_sum_mem)
+    run_algo(inputs, how_sum_tabl)
+    run_algo(inputs, best_sum_mem)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_sum_in_array.py
@@ -0,0 +1,58 @@
+""" tests for the sum_in_array.py functions"""
+import pytest
+from dynamic_prog.sum_in_array import (
+    can_sum_mem,
+    can_sum_tabl,
+    how_sum_mem,
+    how_sum_tabl,
+)
+
+
+@pytest.fixture
+def sum_input_true():
+    """Reurns the input for positive case of sum function tests"""
+
+    return ((0, 7, 10, 300), (5, 4, 3, 7))
+
+
+@pytest.fixture
+def sum_input_false():
+    """Reurns the input for positive case of sum function tests"""
+
+    return ((2, 5, 50, 300), (7, 14))
+
+
+def test_can_sum_true(sum_input_true):
+    """ Tests the memoized can sum function"""
+    target_sums, array = sum_input_true
+
+    for target_sum in target_sums:
+        assert can_sum_mem(target_sum, array) is True
+        assert can_sum_tabl(target_sum, array) is True
+
+
+def test_can_sum_false(sum_input_false):
+    """ Tests the memoized can sum function"""
+    target_sums, array = sum_input_false
+
+    for target_sum in target_sums:
+        assert can_sum_mem(target_sum, array) is False
+        assert can_sum_tabl(target_sum, array) is False
+
+
+def test_how_sum_true(sum_input_true):
+    """Tests positive case of how sum function"""
+    target_sums, array = sum_input_true
+
+    for target_sum in target_sums:
+        assert how_sum_mem(target_sum, array) is not None
+        assert how_sum_tabl(target_sum, array) is not None
+
+
+def test_how_sum_false(sum_input_false):
+    """Tests negative case of how sum function"""
+    target_sums, array = sum_input_false
+
+    for target_sum in target_sums:
+        assert how_sum_mem(target_sum, array) is None
+        assert how_sum_tabl(target_sum, array) is None
